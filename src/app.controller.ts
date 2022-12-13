028d7d4beb87aa59562a787b7e357f1b169593e4
Carlos Silva <miguel21.melo21@gmail.com>
2022-12-13 16:06:56 -0300

    feat: get body and add validation layer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { randomUUID } from 'node:crypto';
+import { CreateNotificationsBody } from './create-notifications-body';
 
 @Controller('app')
 export class AppController {
@@ -12,13 +13,15 @@ export class AppController {
   }
 
   @Post('notifications')
-  async create() {
+  async create(@Body() body: CreateNotificationsBody) {
+    const { recipientId, content, category } = body;
+
     await this.prisma.notification.create({
       data: {
         id: randomUUID(),
-        content: 'Oba! Você recebeu uma nova notificação.',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
